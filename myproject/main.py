77ddbfbd1cedcb0fdd6a5e0dca8a010d57ba9990
Kaustav Pal <kaustavpal007@gmail.com>
2026-03-16 14:47:05 +0530

    update feature

--- myproject/main.py
+++ myproject/main.py
@@ -1,3 +1,35 @@
+import requests
+import sys
 
-def hello(world):
-    return 'Hello, ' + world
+API_KEY = "5Sfc3mhadUEmQlZGjW018FB9ewqY1qkQQcZmJsMp"
+
+def apod():
+    url = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+    data = requests.get(url).json()
+
+    print("\nNASA Astronomy Picture of the Day")
+    print("Title:", data["title"])
+    print("Date:", data["date"])
+    print("Image:", data["url"])
+
+def iss():
+    url = "http://api.open-notify.org/iss-now.json"
+    data = requests.get(url).json()
+
+    print("\nISS Current Location")
+    print("Latitude:", data["iss_position"]["latitude"])
+    print("Longitude:", data["iss_position"]["longitude"])
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: utility-life [apod|iss]")
+        return
+
+    cmd = sys.argv[1]
+
+    if cmd == "apod":
+        apod()
+    elif cmd == "iss":
+        iss()
+    else:
+        print("Unknown command")
--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from os.path import exists
 from setuptools import setup, find_packages
 
-author = 'Kaustav Pal'
-email = 'EMAIL'
-description = 'Utility_Life'
-name = 'myproject'
-year = '2026'
-url = 'URL'
-version = '0.0.1'
+author = "Kaustav Pal"
+email = "kaustavpal007@gmail.com"
+description = "Utility Life Terminal Tool"
+name = "utility-life"
+year = "2026"
+url = "https://github.com/Kaustavpal007/my-project"
+version = "0.0.1"
 
 setup(
     name=name,
@@ -18,17 +19,22 @@ setup(
     url=url,
     version=version,
     packages=find_packages(),
-    package_dir={name: name},
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     description=description,
-    long_description=open('README.md').read() if exists('README.md') else '',
+    long_description=open("README.md").read() if exists("README.md") else "",
     long_description_content_type="text/markdown",
-    install_requires=['sphinx',
-                      ],
+    install_requires=[
+        "requests"
+    ],
     python_requires=">=3.6",
-    classifiers=['Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3',
-                 ],
-    platforms=['ALL'],
+    entry_points={
+        "console_scripts": [
+            "utility-life=myproject.main:main"
+        ]
+    },
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
 )
